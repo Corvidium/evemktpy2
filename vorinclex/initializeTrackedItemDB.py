3f62f7c439b51f94e2e58720c40a7592594014f3
Garrett Soiland <garrettkno3@gmail.com>
2020-04-22 01:38:16 -0700

    cleanup

--- vorinclex/initializeTrackedItemDB.py
+++ vorinclex/initializeTrackedItemDB.py
@@ -32,16 +32,24 @@ numProds = len(jscrape['response'])
 for i in range(0,numProds):
 	try:
 		mktset.update({jscrape['response'][i] : {'ItemName' : itemrefdat[str(jscrape['response'][i])], 'ItemID' : jscrape['response'][i] }})
+		mktset[jscrape['response'][i]].update({'VolOnMkt' : 0})
+		mktset[jscrape['response'][i]].update({'DaysSupplyRemaining' : 0})
+		mktset[jscrape['response'][i]].update({'SellPrices' : []})
+		mktset[jscrape['response'][i]].update({'Competitors' : []})
+		mktset[jscrape['response'][i]].update({'DailyISKFlux' : 0})
+		mktset[jscrape['response'][i]].update({'DailyVol' : 0})
+
+
 		print(itemrefdat[str(jscrape['response'][i])])
 		print(jscrape['response'][i])
 	except KeyError:
-		uniLog('KeyError on key '+str(jscrape['response'][i]))
+		uniLog('updateItems.py KeyError on key '+str(jscrape['response'][i]))
 		continue
 	except:
-		uniLog('unknown error on key '+str(jscrape['response'][i]))
+		uniLog('updateItems.py unknown error on key '+str(jscrape['response'][i]))
 		continue
 
-uniLog('Collation complete. mktset dictionary updated.')
+uniLog('Collation of items to IDs complete. marketSet dictionary updated.')
 print('complete')
 
 #Record in file
@@ -49,6 +57,7 @@ with open(mktconfig.activeitems, 'w') as activeitemsfile:
 	activeitemsfile.write(json.dumps(mktset))
 
 uniLog('Active items written to file '+ mktconfig.activeitems)
+print('Active items written to file '+ mktconfig.activeitems)
 
 time.sleep(30)
 
