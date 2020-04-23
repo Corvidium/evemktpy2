e7c0b4468020dc3e8d8d8f15ed863e5fa084881d
Garrett Soiland <garrettkno3@gmail.com>
2020-04-23 04:12:44 -0700

    fixed date estimation to GMT from local time

--- vorinclex/scrapeMarketOrders.py
+++ vorinclex/scrapeMarketOrders.py
@@ -9,9 +9,13 @@ mkLog("EXECUTION INITIATED")
 
 
 
-
-responseData = compileESIstructureOrders(mktconfig.destoStruct) #esoteria region 10000039
-
+try:
+	responseData = compileESIstructureOrders(mktconfig.destoStruct) #esoteria region 10000039
+except:
+	print('Failed to update structure orders. Sometimes caused by invalid ESI token - put a new one into mktconfig.py by getting it from https://esi.evetech.net/ui/#/Market/get_markets_structures_structure_id')
+	mkLog('Failed to update structure orders. Sometimes caused by invalid ESI token - put a new one into mktconfig.py by getting it from https://esi.evetech.net/ui/#/Market/get_markets_structures_structure_id')
+	time.sleep(300)
+	exit()
 with open(mktconfig.structureOutput+mktconfig.destoStruct+'.json', "w") as file:
 		file.write(json.dumps(responseData))
 
