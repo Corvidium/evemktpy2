1c352964cc335d97f914a0f20f8b57c228c39a04
Garrett Soiland <garrettkno3@gmail.com>
2020-04-26 14:03:46 -0700

    better comments

--- vorinclex/scrapeMarketItems.py
+++ vorinclex/scrapeMarketItems.py
@@ -1,9 +1,10 @@
+#CALLS API TO GET LIST OF ALL GAMEIDS WITH MARKET HISTORY IN TARGET REGION AND PUTS IT IN activeTypesScrape.json
+
 #package import
 import requests, json, datetime, time
 
 #config import
 import mktconfig
-
 
 print(mktconfig.esiurl)
 
@@ -11,15 +12,14 @@ print(mktconfig.esiurl)
 from wrapper import mkLog, callESIorders, callESIhistory, callESItypes, compileESItypes, responseArchive, logArchive
 
 
-print("updateMarketItems.py EXECUTION INITIATED\n")
-mkLog("updateMarketItems.py EXECUTION INITIATED")
+print("scrapeMarketItems.py EXECUTION INITIATED\n")
+mkLog("scrapeMarketItems.py EXECUTION INITIATED")
 
 responseData = compileESItypes(mktconfig.destoRegion) #esoteria region 10000039
-
 logArchive(responseData)
 
+print("scrapeMarketItems.py EXECUTION COMPLETE")
+mkLog("scrapeMarketItems.py EXECUTION COMPLETE")
 
-print("updateMarketItems.py EXECUTION COMPLETE")
-mkLog("updateMarketItems.py EXECUTION COMPLETE")
-
-
+time.sleep(30)
+exit()
